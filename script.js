1fedf36f08a9a831d5fc9772355cf7e648e3a3de
Elmo'minova Mohida <mashinada123@gmail.com>
2024-02-13 09:41:17 +0500

    bill chnage

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const bill =275;
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.30;
 console.log (`the bill was ${bill}, tip was ${tip}, and the otal value ${bill + tip}`);

f91c325a7dd441c44a4b220fd4b7ee12cd7490be
Elmo'minova Mohida <mashinada123@gmail.com>
2024-02-13 09:45:53 +0500

    a

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const bill =275;
+const bill = 275;
 const tip = bill >= 50 && bill <= 300 ? bill * 0.25 : bill * 0.30;
-console.log (`the bill was ${bill}, tip was ${tip}, and the otal value ${bill + tip}`);
+console.log(`the bill was ${bill}, tip was ${tip}, and the otal value ${bill + tip}`);

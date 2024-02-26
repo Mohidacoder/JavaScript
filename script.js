96b0480aa9188f4eb660b88a86a46aa577bad4dd
Mohida <158798918+Mohidacoder@users.noreply.github.com>
2024-02-26 15:37:30 +0500

    java script

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-const bill = 275;
+/*const bill = 275;
 const tip = bill >= 50 && bill <= 300 ? bill * 0.25 : bill * 0.30;
-console.log(`the bill was ${bill}, tip was ${tip}, and the otal value ${bill + tip}`);
+console.log(`the bill was ${bill}, tip was ${tip}, and the otal value ${bill + tip}`);*/
+
+"use script";
+const calctip = function (bill){
+    const tip = bill >= 50 && bill >= 300 ? bill * 0.15 : bill * 0.2;
+    return tip;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++){
+    const tip = calctip(bills[i]);
+    tips.push(tip);
+    totals.push(bills[i] + tip);
+};
+
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i=0; i<arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+};
+
+console.log(calcAverage(bills));
+console.log(calcAverage(tips));
+console.log(calcAverage(totals));
+
